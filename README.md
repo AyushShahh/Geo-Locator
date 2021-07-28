0f0a2f6d9e5f52bacfe4ff43ba402760a63dff69
Ayush Shah <shahayush001@gmail.com>
2021-07-28 16:02:16 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Geo-Locator
- Get your geo-location details with live location on map
+# Geo-Locator ![logo](assets/favicons/favicon-32x32.png)
+
+[![Views](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2FAyushShahh%2FGeo-Locator&count_bg=%23000000&title_bg=%23555555&icon=github.svg&icon_color=%23E7E7E7&title=views&edge_flat=false)](https://hits.seeyoufarm.com)
+[![GitHub stars](https://img.shields.io/github/stars/AyushShahh/Geo-Locator)](https://github.com/AyushShahh/Geo-Locator/stargazers)
+[![GitHub forks](https://img.shields.io/github/forks/AyushShahh/Geo-Locator)](https://github.com/AyushShahh/Geo-Locator/network)
+[![GitHub issues](https://img.shields.io/github/issues/AyushShahh/Geo-Locator)](https://github.com/AyushShahh/Geo-Locator/issues)
+[![Twitter](https://img.shields.io/twitter/url?logoColor=black&style=social&url=https%3A%2F%2Fgithub.com%2FAyushShahh%2FGeo-Locator)](https://twitter.com/intent/tweet?text=Woah!%20This%20is%20cool.&url=https%3A%2F%2Fgithub.com%2FAyushShahh%2FGeo-Locator)
+
+Get your **geo-location** details with **live position** on map.<br>
+Check the live site **[here](https://geo-locator.netlify.app)**.
---
+++ scripts/location.js
@@ -0,0 +1,53 @@
+var x = document.getElementById("loc");
+var y = document.getElementById("mapholder");
+var z = document.getElementById("info");
+const map_api = "YOUR_KEY";
+const geoinfo_token = "YOUR_TOKEN";
+
+function getLocation() {
+   if (navigator.geolocation) {
+     navigator.geolocation.getCurrentPosition(showPosition,showError);
+   } else { 
+      z.innerHTML = "<br><br><b>Geo-Location is not supported on this browser.</b>";
+   }
+ }
+
+function showPosition(position) {
+   var latlon = position.coords.latitude + ',' + position.coords.longitude;
+
+   x.innerHTML = "<b>Latitude: </b>" + position.coords.latitude + "<br><b>Longitude: </b>" + position.coords.longitude;
+
+   var img_url = "https://www.google.com/maps/embed/v1/place?key="+map_api+"&q="+latlon+"&maptype=satellite&center="+latlon+"&zoom=18";
+
+   y.innerHTML = "<iframe style='border:0' loading='lazy' allowfullscreen src='"+img_url+"'>";
+}
+
+function showError(error) {
+   switch(error.code) {
+      case error.PERMISSION_DENIED:
+         z.innerHTML = "<br><br><b>User denied the request for location.<br>Click allow to see your location on map.</b>"
+         break;
+      case error.POSITION_UNAVAILABLE:
+         z.innerHTML = "<br><br><b>Map is currently unavailable.<br>Turn on your Location(GPS).</b>"
+         break;
+      case error.TIMEOUT:
+         z.innerHTML = "<br><br><b>The request for map location timed out.</b>"
+         break;
+      case error.UNKNOWN_ERROR:
+         z.innerHTML = "<br><br><b>An unknown error occurred.</b>"
+         break;
+   }
+}
+
+$.getJSON('https://geolocation-db.com/json/')
+.done (function(location) {
+   $('#country').html(location.country_name);
+   $('#ip').html(location.IPv4);
+});
+
+$.get("https://ipinfo.io/json?token="+geoinfo_token+"", function (response) {
+   $("#address").html(response.city + ", " + response.region);
+   $("#postal").html(response.postal);
+   $("#org").html(response.org);
+   $("#loc").html("<b>Latitude/Longitude: </b>"+ response.loc);
+}, "jsonp");
